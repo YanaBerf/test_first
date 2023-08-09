9280fffc381ddbb9d9029e6708fa25ca2c51b543
[YanaB] <[tudabae@gmail.com]>
2023-08-09 15:45:46 +0300

    задача вывод массива строк, длина которых меньше или равно 3

---
+++ task_one_/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите символы или слова через пробел: ");
+string[] arr = Console.ReadLine().Split();
+
+PrintArray(GetArray(arr));
+
+string[] GetArray(string[] array){
+    string[] result = new string[array.Length];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            result[j] = array[i];
+            j++;
+        }
+    } return result;
+   //Console.WriteLine(String.Join(" ", result));
+} 
+void PrintArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
